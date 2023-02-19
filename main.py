c485b5cb622a680a50655b4aa42ad3d4caf1904e
EduardsJanitis221RDB271 <eduards.janitis@edu.rtu.lv>
2023-02-19 21:35:07 +0200

    commit

--- main.py
+++ main.py
@@ -14,16 +14,25 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
-            pass
-
+            opening_brackets_stack.append(Bracket(next, i+1))
+        pass
         if next in ")]}":
             # Process closing bracket, write your code here
-            pass
+            if not opening_brackets_stack:
+                return i+1
+            x=opening_brackets_stack.pop()
+            if not are_matching(x.char,next):
+                return i+1
+    if opening_brackets_stack:
+        return opening_brackets_stack[0].position
+    return "Success"
+    #pass
 
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
+    print(mismatch)
     # Printing answer, write your code here
 
 
